8b331d91cd0420e69077bfe7c070b68853f71fcd
ShevHet <141827983+ShevHet@users.noreply.github.com>
2026-02-10 23:42:07 +0300

    Add POST /tasks with DTO validation and 201 Created

--- src/TaskTracker.Api/Program.cs
+++ src/TaskTracker.Api/Program.cs
@@ -2,18 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-// Swagger services (§£§¡§¨§¯§°)
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger middleware (§£§¡§¨§¯§°)
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// §£§¡§¨§¯§°: §Ö§ã§Ý§Ú §å §ä§Ö§Ò§ñ §ä§à§Ý§î§Ü§à http §Ú §â§å§Ô§Ñ§Ö§ä§ã§ñ §ß§Ñ https ¡ª §Ó§â§Ö§Þ§Ö§ß§ß§à §Ó§í§Ü§Ý§ð§é§Ú §â§Ö§Õ§Ú§â§Ö§Ü§ä
-// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
