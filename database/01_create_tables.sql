3435053b7bb3fac64f3f9cb5f059c851f6d2f2d6
ShevHet <141827983+ShevHet@users.noreply.github.com>
2026-03-08 23:52:19 +0300

    Неделя 4 День 3: добавить примеры JOIN и GROUP BY запросов

--- database/01_create_tables.sql
+++ database/01_create_tables.sql
@@ -1,29 +1,29 @@
-SET  NOCOUNT ON;
-GO 
-
-IF OBJECT_ID(N'dbo.Project', N'U') IS NULL
+GO
+IF OBJECT_ID(N'dbo.Projects', N'U') IS NULL
 BEGIN
-	CREATE TABLE dbo.Projects(
-		Id int IDENTITY(1,1) NOT NULL CONSTRAINT PK_Project PRIMARY KEY,
+	CREATE TABLE dbo.Projects
+		(
+		Id int IDENTITY(1,1) CONSTRAINT PK_Projects PRIMARY KEY,
 		Name nvarchar(200) NOT NULL,
 		Description nvarchar(1000) NULL,
-		CreatedAtUTC datetime2(7) NOT NULL CONSTRAINT DF_Project_CreatedAtaUtc DEFAULT (sysutdatetime()),
-		UpdateAtUtc datetime(7) NULL
-		);
+		CreatedAtUtc datetime2(7) NOT NULL CONSTRAINT DF_Projects_CreatedAtUtc DEFAULT (sysutcdatetime()),
+		UpdateAtUtc datetime2(7) Null
+	);
 END;
-Go
+
+GO
 
 IF OBJECT_ID(N'dbo.Tasks', N'U') IS NULL
 BEGIN
-	CREATE TABLE dbo.Tasks (
-		Id int IDENTITY(1,1) NOT NULL, CONSTRAINT PK_Tasks PRIMARY KEY,
-		ProjectID int NOT NULL,
-		Title nvachar(200) NOT NULL,
-		Description nvachar(2000) NULL,
-		Status int NOT NULL,
-		DueDateUtc datetime2(7) NULL,
-		CreatedAtUtc datetime2(7) NOT NULL CONSTRAINT DF_Tasks_CreatedAtUtc DEFAULT (sysutcdatetime()),
-		UpdateAtUtc datetime2(7) NULL
-	);
+    CREATE TABLE dbo.Tasks (
+        Id int IDENTITY(1,1) NOT NULL CONSTRAINT PK_Tasks PRIMARY KEY,
+        ProjectId int NOT NULL,
+        Title nvarchar(200) NOT NULL,
+        Description nvarchar(2000) NULL,
+        Status int NOT NULL,
+        DueDateUtc datetime2(7) NULL,
+        CreatedAtUtc datetime2(7) NOT NULL CONSTRAINT DF_Tasks_CreatedAtUtc DEFAULT (sysutcdatetime()),
+        UpdatedAtUtc datetime2(7) NULL
+    );
 END;
 GO
