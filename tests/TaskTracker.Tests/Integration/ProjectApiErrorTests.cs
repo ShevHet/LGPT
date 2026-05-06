e8b47cdcba585d6cefde01d780b3c6b237093a1c
ShevHet <141827983+ShevHet@users.noreply.github.com>
2026-05-06 22:42:51 +0300

    Добавлены интеграционные API-тесты для 400 и 404

--- tests/TaskTracker.Tests/Integration/ProjectApiErrorTests.cs
+++ tests/TaskTracker.Tests/Integration/ProjectApiErrorTests.cs
@@ -1,6 +1,43 @@
+<<<<<<< HEAD
 ﻿namespace TaskTracker.Tests.Integration
 {
     public class ProjectApiErrorTests
     {
+=======
+﻿using System.Net;
+
+namespace TaskTracker.Tests.Integration
+{
+    public sealed class ProjectApiErrorTests : IClassFixture<ApiFactory>
+    {
+        private readonly ApiFactory _factory;
+        private readonly HttpClient _client;
+
+        public ProjectApiErrorTests(ApiFactory factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetProjectById_ShouldReturnBadRequest_WhenIdNotPositive()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var response = await _client.GetAsync($"/projects/0");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetProjectById_ShouldReturnNotFound_WhenProjectDoesNotExists()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var response = await _client.GetAsync("/projects/999999");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+>>>>>>> 2244c33 (Добавлены интеграционные API-тесты для 400 и 404)
     }
 }
--- tests/TaskTracker.Tests/Integration/TaskApiErrorTests.cs
+++ tests/TaskTracker.Tests/Integration/TaskApiErrorTests.cs
@@ -1,6 +1,51 @@
+<<<<<<< HEAD
 ﻿namespace TaskTracker.Tests.Integration
 {
     public class TaskApiErrorTests
     {
+=======
+﻿using System.Net;
+using System.Net.Http.Json;
+
+
+namespace TaskTracker.Tests.Integration
+{
+    public sealed class TaskApiErrorTests : IClassFixture<ApiFactory>
+    {
+        private readonly ApiFactory _factory;
+        private readonly HttpClient _client;
+
+        public TaskApiErrorTests(ApiFactory factory)
+        {
+            _factory = factory;
+            _client = new HttpClient();
+        }
+
+        [Fact]
+        public async Task CreateTask_ShouldReturnBadRequest_WhenRequestIsInvalid()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var response = await _client.PostAsJsonAsync("/tasks", new
+            {
+                projectId = 0,
+                title = "",
+                description = "Invalid task",
+                status = "New"
+            });
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetTaskById_ShouldReturnNotFound_WhenTaskDoesNotExist()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var response = await _client.GetAsync("/tasks/999999");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+>>>>>>> 2244c33 (Добавлены интеграционные API-тесты для 400 и 404)
     }
 }
